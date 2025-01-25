1ab9a27bf8d557effbe200eb985c50b0cf90478f
Charles Donizete Alves dos Santos <82051427+CharlesDonizete@users.noreply.github.com>
2025-01-25 11:21:37 -0300

    Aula do dia 25/01

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,5 +1,9 @@
 using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +22,7 @@ builder.Services.AddSwaggerGen(x =>
     x.CustomSchemaIds(n => n.FullName);
 });
 
-builder.Services.AddTransient<Handler>();
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 
 var app = builder.Build();
 
@@ -27,44 +31,47 @@ app.UseSwaggerUI();
 
 app.MapPost(
     "/v1/categories",
-    (Request request, Handler handler)
-        => handler.Handle(request))
+    async (CreateCategoryRequest request, ICategoryHandler handler)
+        => await handler.CreateAsync(request))
     .WithName("Categories: Create")
     .WithSummary("Cria uma nova categoria")
-    .Produces<Response>();
+    .Produces<Response<Category?>>();
+
+app.MapPut(
+    "/v1/categories/{id}",
+    async (long id, UpdateCategoryRequest request, ICategoryHandler handler)
+        => 
+        { 
+            request.Id = id; 
+            return await handler.UpdateAsync(request);
+        })        
+    .WithName("Categories: Update")
+    .WithSummary("Atualiza uma categoria")
+    .Produces<Response<Category?>>();
+
+app.MapDelete(
+    "/v1/categories/{id}",
+    async (long id, ICategoryHandler handler)
+        =>
+        { 
+            var request = new DeleteCategoryRequest { Id = id };            
+            return await handler.DeleteAsync(request); 
+        })
+    .WithName("Categories: Delete")
+    .WithSummary("Exclui uma categoria")
+    .Produces<Response<Category?>>();
+
+app.MapGet(
+    "/v1/categories/{id}",
+    async (long id, ICategoryHandler handler)
+        =>
+    {
+        var request = new GetCategoryByIdRequest { Id = id };
+        return await handler.GetByIdAsync(request);
+    })
+    .WithName("Categories: Get by Id")
+    .WithSummary("Retorna uma categoria")
+    .Produces<Response<Category?>>();
 
 app.Run();
-
-public class Request
-{
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-}
-
-public class Response
-{
-    public long Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-}
-
-public class Handler(AppDbContext context)
-{
-    public Response Handle(Request request)
-    {
-        var category = new Category
-        {
-            Title = request.Title,
-            Description = request.Description
-        };
-
-        context.Categories.Add(category);
-        context.SaveChanges();
-
-        return new Response
-        {
-            Id = category.Id,
-            Title = category.Title
-        };
-    }
-}
 
