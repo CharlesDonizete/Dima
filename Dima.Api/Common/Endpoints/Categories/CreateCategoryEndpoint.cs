9ea10e490bc89e9a693a605d00607cad118c8c1a
Charles Donizete Alves dos Santos <82051427+CharlesDonizete@users.noreply.github.com>
2025-02-01 11:35:42 -0300

    Aula do dia 01/02

---
+++ Dima.Api/Common/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Common.Endpoints.Categories
+{
+    public class CreateCategoryEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapPost("/", HandleAsync)
+            .WithName("Categories: Create")
+            .WithSummary("Cria uma categoria")
+            .WithDescription("Cria uma categoria")
+            .WithOrder(1)
+            .Produces<Response<Category?>>();
+
+        private static async Task<IResult> HandleAsync(
+            ICategoryHandler handler,
+            CreateCategoryRequest request
+            )
+        {
+            var result = await handler.CreateAsync( request );
+          
+            return result.IsSuccess 
+                ? TypedResults.Created($"/{result.Data?.Id}", result) 
+                : TypedResults.BadRequest(result);
+        }
+    }
+}
--- Dima.Api/Handlers/CategoryHandler.cs
+++ Dima.Api/Handlers/CategoryHandler.cs
@@ -51,9 +51,33 @@ public class CategoryHandler(AppDbContext context) : ICategoryHandler
         }
     }
 
-    public Task<Response<List<Category>>> GetAllAsync(GetAllCategoriesRequest request)
+    public async Task<PagedResponse<List<Category>>> GetAllAsync(GetAllCategoriesRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var query = context
+           .Categories
+           .AsNoTracking()
+           .Where(x => x.UserId == request.UserId)
+           .OrderBy(x => x.Title);
+
+            var categories = await query
+                .Skip(( request.PageNumber -1) * request.PageSize)
+                .Take(request.PageSize)
+                .ToListAsync();
+
+            var count = await query.CountAsync();
+
+            return new PagedResponse<List<Category>>(
+                categories,
+                count,
+                request.PageNumber,
+                request.PageSize);
+        }
+        catch (Exception)
+        {
+            return new PagedResponse<List<Category>>(null, 500, "Não foi possível consulta as categorias.");
+        }       
     }
 
     public async Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
